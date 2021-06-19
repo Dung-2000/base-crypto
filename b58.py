343220774392e3a57c9711c67d3fdcd363679005
Dung@ASUS-n580vd <e883045t100@gmail.com>
2021-06-19 20:55:42 +0800

    functionalize b58.py

--- b58.py
+++ b58.py
@@ -1,19 +1,24 @@
-plain = 'Hello'
-table = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopgrstuvwxyz'
-assert len(table) == 58
+# example: https://oranwind.org/post-post-7/
+def myb58encode(plain):
+    table = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopgrstuvwxyz'
+    assert len(table) == 58
 
-asc_result = 0
-for i in range(len(plain)):
-    temp = ord(plain[i]) * (2**(8*(len(plain) - i - 1)))
-    asc_result += temp
+    asc_result = 0
+    for i in range(len(plain)):
+        temp = ord(plain[i]) * (2**(8*(len(plain) - i - 1)))
+        asc_result += temp
 
-out_str = ''
-while True:
-    num1 = asc_result // 58
-    num2 = asc_result % 58
-    #print(num1,num2)
-    out_str += table[num2]
-    asc_result = num1
-    if num1 == 0:
-        break
-print(''.join(reversed(out_str)))
+    out_str = ''
+    while True:
+        num1 = asc_result // 58
+        num2 = asc_result % 58
+        #print(num1,num2)
+        out_str += table[num2]
+        asc_result = num1
+        if num1 == 0:
+            break
+    return (''.join(reversed(out_str)))
+
+if __name__ == '__main__':
+    plain = 'Hellow'
+    print(myb58encode(plain=plain))
