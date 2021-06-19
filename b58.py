02006048c872c7adcfdfc9fe6673777f318567f4
Dung@ASUS-n580vd <e883045t100@gmail.com>
2021-06-19 20:49:27 +0800

    add b58.py

---
+++ b58.py
@@ -0,0 +1,19 @@
+plain = 'Hello'
+table = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopgrstuvwxyz'
+assert len(table) == 58
+
+asc_result = 0
+for i in range(len(plain)):
+    temp = ord(plain[i]) * (2**(8*(len(plain) - i - 1)))
+    asc_result += temp
+
+out_str = ''
+while True:
+    num1 = asc_result // 58
+    num2 = asc_result % 58
+    #print(num1,num2)
+    out_str += table[num2]
+    asc_result = num1
+    if num1 == 0:
+        break
+print(''.join(reversed(out_str)))
